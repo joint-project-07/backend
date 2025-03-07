004b3f21b175b8b351ab8ac07e009f53876cfc00
BlueLight <dlrkdmf10@gmail.com>
2025-03-07 18:01:21 +0900

    💡chore: users image 를 url 로 저장

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -9,14 +11,14 @@ from common.models import BaseModel
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, is_shelter=False, **extra_fields):
         # 소셜 로그인을 지원하는 경우, 처음에 비밀번호 없이 계정을 생성할 수 있기 때문에 password=None
         if not email:
             raise ValueError("이메일은 필수입니다.")
         email = self.normalize_email(
             email
         )  # normalize_email: 이메일을 일관된 형식으로 변환하는 메서드
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, is_shelter=is_shelter, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -33,20 +35,14 @@ class SocialType(models.TextChoices):
     KAKAO = "kakao", "Kakao"
 
 
-def user_profile_image_path(instance, filename):
-    return f"users/{instance.id}/{filename}"
-
-
 class User(AbstractBaseUser, PermissionsMixin, BaseModel):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True, max_length=255, null=False)
     password = models.CharField(max_length=255, null=False)
     name = models.CharField(max_length=100, null=False)
     birth_date = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=False)
-    profile_image = models.ImageField(
-        upload_to=user_profile_image_path, null=True, blank=True
-    )
+    contact_number = models.CharField(max_length=20, null=False)
+    profile_image_url = models.CharField(max_length=500, null=True, blank=True)
     social_type = models.CharField(
         max_length=10, choices=SocialType.choices, null=False
     )
