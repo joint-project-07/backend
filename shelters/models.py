47ea805a48d1d2e8e79a4d6cafde54405c5ea69e
BlueLight <dlrkdmf10@gmail.com>
2025-03-07 16:43:24 +0900

    💡chore: shelters/models.py 수정

--- shelters/models.py
+++ shelters/models.py
@@ -1,7 +1,9 @@
 import uuid
 
 from django.db import models
+
 from common.models import BaseModel
+from users.models import User
 
 
 class RegionChoices(models.TextChoices):
@@ -30,25 +32,69 @@ class ShelterTypeChoices(models.TextChoices):
     NON_PROFIT = "non_profit", "Non-Profit"
 
 
+class ShelterFileTypeChoices(models.TextChoices):
+    PROFILE = "profile", "Profile"
+    GENERAL = "general", "General"
+    BUSINESS_LICENSE = "business_license", "Business License"
+
+
+def shelter_images_path(instance, filename):
+    shelter_id = instance.shelter.id if instance.shelter.id else str(uuid.uuid4())
+    return f"shelters/{shelter_id}/images/{filename}w"
+
+
+def shelter_file_path(instance, filename):
+    shelter_id = instance.shelter.id if instance.shelter.id else str(uuid.uuid4())
+    return f"shelters/{shelter_id}/license/{filename}"
+
+
 class Shelter(BaseModel):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False)
     address = models.CharField(max_length=255, null=False)
-    region = models.CharField(
-        max_length=10, choices=RegionChoices.choices(), null=False
-    )
+    region = models.CharField(max_length=10, choices=RegionChoices.choices, null=False)
     contact_number = models.CharField(max_length=20, null=False)
     shelter_type = models.CharField(
-        max_length=20, choices=ShelterTypeChoices.choices(), null=False
+        max_length=20, choices=ShelterTypeChoices.choices, null=False
     )
-    owner_name = models.CharField(max_length=100, null=False)
-    profile_image = models.URLField(max_length=255)
     business_registration_number = models.CharField(max_length=20, null=False)
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=False)
-    business_license_file = models.FileField(upload_to='business_licenses/', null=False)
+    business_license_file = models.FileField(upload_to=shelter_file_path, null=False)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="shelter")
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = "shelters"
+
+
+class ShelterImage(BaseModel):
+    id = models.AutoField(primary_key=True)
+    shelter = models.ForeignKey(
+        Shelter, on_delete=models.CASCADE, related_name="images"
+    )
+    image_type = models.CharField(
+        max_length=20,
+        choices=ShelterFileTypeChoices.choices,
+        default=ShelterFileTypeChoices.GENERAL,  # 기본값: 일반 이미지
+    )
+    image = models.ImageField(upload_to=shelter_images_path, null=False)
+
+    def __str__(self):
+        return f"{self.shelter.name} - {self.image_type} - Image {self.id}"
+
+    class Meta:
+        db_table = "shelters_images"
+
+
+class ShelterFile(BaseModel):
+    id = models.AutoField(primary_key=True)
+    shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE, related_name="files")
+    file = models.FileField(upload_to=shelter_file_path, null=False)
+
+    def __str__(self):
+        return f"{self.shelter.name} - Business License"
+
+    class Meta:
+        db_table = "shelters_files"
