1847002c32c849a7812fc7622afa1713d7aee811
BlueLight <dlrkdmf10@gmail.com>
2025-03-10 19:42:50 +0900

    💡chore: applications model 수정

--- applications/models.py
+++ applications/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from volunteer.models import VolunteerActivity
 
 from common.models import BaseModel
+from recruitments.models import Recruitment
+from shelters.models import Shelter
 from users.models import User
 
 
-class VolunteerApplication(BaseModel):
+class Application(BaseModel):
     STATUS_CHOICES = [
         ("pending", "승인 대기"),
         ("approved", "승인 완료"),
@@ -15,11 +16,14 @@ class VolunteerApplication(BaseModel):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="applications"
     )
-    volunteer_activity = models.ForeignKey(
-        VolunteerActivity, on_delete=models.CASCADE, related_name="applications"
+    recruitment = models.ForeignKey(
+        Recruitment, on_delete=models.CASCADE, related_name="applications"
+    )
+    shelter = models.ForeignKey(
+        Shelter, on_delete=models.CASCADE, related_name="applications"
     )
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
     rejected_reason = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.volunteer_activity.activity_date} ({self.get_status_display()})"
+        return f"{self.user.name} - {self.recruitment.date} ({self.status})"
