c8dde3379904e2ff59167ae5aaaa2db5bab00d4b
BlueLight <dlrkdmf10@gmail.com>
2025-03-10 17:12:01 +0900

    🚚build: volunteer_history 를 histories 로 변경

--- histories/apps.py
+++ histories/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VolunteerHistoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "volunteer_history"
+    name = "histories"
--- histories/models.py
+++ histories/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 from common.models import BaseModel
 from users.models import User
-from volunteer_application.models import VolunteerApplication
+from applications.models import VolunteerApplication
 
 
 class VolunteerHistory(BaseModel):
     RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]  # 1~5점
 
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="volunteer_history"
+        User, on_delete=models.CASCADE, related_name="histories"
     )
     application = models.ForeignKey(
         VolunteerApplication, on_delete=models.CASCADE, related_name="history"
