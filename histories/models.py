ca15444e10b60eabc9d44b2407f501c8f99e395a
BlueLight <dlrkdmf10@gmail.com>
2025-03-10 19:44:27 +0900

    💡chore: histories model 수정

--- histories/models.py
+++ histories/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
-from applications.models import VolunteerApplication
+from applications.models import Application
 from common.models import BaseModel
+from shelters.models import Shelter
 from users.models import User
 
 
-class VolunteerHistory(BaseModel):
+class History(BaseModel):
     RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]  # 1~5점
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="histories")
+    shelter = models.ForeignKey(
+        Shelter, on_delete=models.CASCADE, related_name="histories"
+    )
     application = models.ForeignKey(
-        VolunteerApplication, on_delete=models.CASCADE, related_name="history"
+        Application, on_delete=models.CASCADE, related_name="history"
     )
     rating = models.IntegerField(choices=RATING_CHOICES, default=1)  # 만족도 평가
 
     def __str__(self):
-        return f"{self.user.username} - {self.application.recruitment.date} (완료됨)"
+        return f"{self.user.name} - {self.application.recruitment.date} (완료됨)"
