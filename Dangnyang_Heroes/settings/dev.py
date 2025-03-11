da2831e8e8707a6527dff4ebadf58d5eecd93946
SOLBI <thfqlsla35@gmail.com>
2025-03-11 10:49:43 +0900

    ✨feat: spectacularSwagger

--- Dangnyang_Heroes/settings/dev.py
+++ Dangnyang_Heroes/settings/dev.py
@@ -9,3 +9,16 @@ SIMPLE_JWT.update(
         "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     }
 )
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
+}
