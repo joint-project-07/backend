a5095f4c5df336e9656171a483b73b856af896fa
SOLBI <thfqlsla35@gmail.com>
2025-03-13 14:36:52 +0900

    ✨feat: users/urls.py 추가

--- Dangnyang_Heroes/urls.py
+++ Dangnyang_Heroes/urls.py
@@ -25,6 +25,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/users/", include("users.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
     path(
--- users/urls.py
+++ users/urls.py
@@ -12,12 +12,12 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", SignupView.as_view(), name=""),
-    path("", ShelterSignupView.as_view(), name=""),
-    path("", EmailLoginView.as_view(), name=""),
-    path("", KakaoLoginView.as_view(), name=""),
-    path("", FindEmailView.as_view(), name=""),
-    path("", ResetPasswordView.as_view(), name=""),
-    path("", UserView.as_view(), name=""),
-    path("", LogoutView.as_view(), name=""),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("signup/shelter/", ShelterSignupView.as_view(), name="signup-shelter"),
+    path("login/", EmailLoginView.as_view(), name="login"),
+    path("kakao-login/", KakaoLoginView.as_view(), name="kakao-login"),
+    path("find-email/", FindEmailView.as_view(), name="find-email"),
+    path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
+    path("me/", UserView.as_view(), name="me"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
