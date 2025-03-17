7c7830d3c91c700aaa4ca50acb3ef47f9f54ee4d
Kiyeoun <agumon70@naver.com>
2025-03-17 15:01:04 +0900

    ⚡️ Ulrs 수정

--- shelters/urls.py
+++ shelters/urls.py
@@ -10,13 +10,13 @@ from .views import (
 
 urlpatterns = [
     # 🧀 보호소 전체 목록 조회 (GET)
-    path("shelters/", ShelterListView.as_view(), name="shelter-list"),
+    path("", ShelterListView.as_view(), name="shelter-list"),
     # 🧀 보호소 검색 (GET)
-    path("shelters/search/", ShelterSearchView.as_view(), name="shelter-search"),
+    path("search/", ShelterSearchView.as_view(), name="shelter-search"),
     # 🧀 보호소 상세 조회 (GET)
-    path("shelters/<int:pk>/", ShelterDetailView.as_view(), name="shelter-detail"),
+    path("<int:pk>/", ShelterDetailView.as_view(), name="shelter-detail"),
     # 🧀 보호소 정보 수정 (PATCH)
-    path("shelters/me/", ShelterUpdateView.as_view(), name="shelter-update"),
+    path("me/", ShelterUpdateView.as_view(), name="shelter-update"),
     # 🧀 보호소 정보 조회 (GET)
-    path("shelters/me/", MyShelterDetailView.as_view(), name="my-shelter-detail"),
+    path("me/", MyShelterDetailView.as_view(), name="my-shelter-detail"),
 ]
