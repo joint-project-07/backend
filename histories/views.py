cb1f8bc5a049672f3461c38127d960eb104efbfe
BlueLight <dlrkdmf10@gmail.com>
2025-03-20 19:59:24 +0900

    🐛fix: 보호소에 의해 완료된 봉사활동 기록을 history 에 저장

--- histories/views.py
+++ histories/views.py
@@ -9,18 +9,20 @@ from histories.serializers import HistoryRatingSerializer, HistorySerializer
 
 
 class HistoryAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
+    # 봉사자가 완료한 봉사 이력 조회
     @extend_schema(
-        summary="봉사활동 이력 조회",
-        description="봉사활동 이력을 조회합니다.",
+        summary="완료한 봉사활동 이력 조회",
+        description="봉사자가 완료한 봉사활동 이력을 조회합니다.",
         responses={
             200: HistorySerializer(many=True),
-            404: {"example": {"detail": "완료한 봉사활동 기록이 없습니다."}},
+            404: {"example": {"error": "완료한 봉사활동 기록이 없습니다."}},
         },
     )
     def get(self, request):
-        histories = History.objects.filter(user=request.user)
+        histories = History.objects.filter(
+            user=request.user, application__status="attended"
+        )
+
         if not histories.exists():
             return Response(
                 {"message": "완료한 봉사활동 기록이 없습니다."},
@@ -32,22 +34,28 @@ class HistoryAPIView(APIView):
 
 
 class HistoryRatingAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
+    # 보호소 평가
     @extend_schema(
-        summary="봉사활동 평가",
-        description="봉사활동에 대한 만족도 평가를 1 ~ 5 까지 등록합니다.",
+        summary="보호소 평가",
+        description="봉사자가 자신이 완료한 봉사활동을 기반으로 보호소에 점수를 부여합니다.",
         responses={
             200: HistoryRatingSerializer,
-            404: {"example": {"detail": "해당 봉사 이력을 찾을 수 없습니다."}},
+            404: {"example": {"error": "해당 봉사 이력을 찾을 수 없습니다."}},
         },
     )
     def post(self, request, history_id):
         try:
             history = History.objects.get(id=history_id, user=request.user)
+
+            if history.application.status != "attended":
+                return Response(
+                    {"error": "완료된 봉사활동에 대해서만 평가할 수 있습니다."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
         except History.DoesNotExist:
             return Response(
-                {"message": "해당 봉사 이력을 찾을 수 없습니다."},
+                {"error": "해당 봉사 이력을 찾을 수 없습니다."},
                 status=status.HTTP_404_NOT_FOUND,
             )
 
