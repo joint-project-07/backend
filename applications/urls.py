d35fb215a922b151eca8870fec0d19a0a2b8a420
BlueLight <dlrkdmf10@gmail.com>
2025-03-25 15:35:56 +0900

    ✅test: run black

--- applications/urls.py
+++ applications/urls.py
@@ -11,17 +11,25 @@ from applications.views import (
 
 urlpatterns = [
     path("", ApplicationListCreateView.as_view(), name="application-list-create"),
-    path("<int:application_id>/", ApplicationDetailView.as_view(), name="application-detail"),
+    path(
+        "<int:application_id>/",
+        ApplicationDetailView.as_view(),
+        name="application-detail",
+    ),
     path(
         "<int:application_id>/approved/",
         ApplicationApproveRejectView.as_view(),
         name="application-approve",
     ),
     path(
-        "<int:application_id>/rejected/", ApplicationRejectView.as_view(), name="application-reject"
+        "<int:application_id>/rejected/",
+        ApplicationRejectView.as_view(),
+        name="application-reject",
     ),
     path(
-        "<int:application_id>/attended/", ApplicationAttendView.as_view(), name="application-attend"
+        "<int:application_id>/attended/",
+        ApplicationAttendView.as_view(),
+        name="application-attend",
     ),
     path(
         "<int:application_id>/absence/",
